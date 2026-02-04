be587572f8576efd9440fd0e7d3d185eb5ee97c1
76716 <farhannayyar77@gmail.com>
2026-02-04 00:54:27 +0000

    Draw Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-// Define the necessary shaders to allow drawing
+// Define the necessary shaders to allow drawing --
 
 const char* vertexShaderSrc = "#version 330 core\n"
 	"layout(location = 0) in vec3 aPos;\n"
@@ -26,8 +26,10 @@ const char* vertexShaderSrc = "#version 330 core\n"
 const char* fragShaderSrc = "#version 330 core\n"
 	"out vec4 fragColour;\n"
 	"void main() {\n"
-	"	fragColour = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"	fragColour = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
 	"}\0";
+
+// --
 
 // Callback in case the window's size changes
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
@@ -49,9 +51,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create window
 	GLFWwindow* window = glfwCreateWindow(800, 600, "balls", NULL, NULL);
-
 	if (!window) {
 		glfwTerminate();
 		return -1;
@@ -66,23 +66,8 @@ int main() {
 	// Not particularly necessary
 	glViewport(0, 0, 800, 600);
 
-	// A set of 3D vertices that we intend to draw
-	// These set of vectors form a simple triangle
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
-	};
-
-	// Declare a buffer object to store vertices in the GPU (faster read-writes)
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 	// Declare Vertex Shader to help GL determine vertex positions
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
 	glCompileShader(vertexShader);
 
@@ -98,8 +83,7 @@ int main() {
 	// --
 
 	// Declare Fragment Shader to determine each pixel's colour as vertices are mapped onto the window
-	unsigned int fragShader;
-	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	unsigned int fragShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
 	glCompileShader(fragShader);
 
@@ -125,31 +109,54 @@ int main() {
 	if (!success) {
     	glGetProgramInfoLog(shader, 512, NULL, errorLog);
 	}
-	// -- 
-
-	// Activate the linked shader and get rid of the old ones
-	glUseProgram(shader);
+	
+	// Get rid of old shaders after linking (not needed anymore)
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragShader);
+	// --
 
-	// Tells GL how to interpret vertex data
+	// Setup vertex data
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	// Declare a buffer object to store vertices in the GPU
+	// Declare an array object to store VBO configurations
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
+	// Select it, put shit into it, tell GL how to interpret it, enable it
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	
-	// -----------
-	// UNFINISHED - need to bind VAO
-	// -----------
+	glEnableVertexAttribArray(0);
+
+	// Deselect everything
+	// not even necessary in the slighest btw (asides from maybe unbinding the vbo)
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
-		// rendering here
+		glUseProgram(shader);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// Safely delete all the resources that were allocated to GLFW and terminate
+	// get rid of all deez blokes
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shader);
+
 	glfwTerminate();
 	return 0;
 }
